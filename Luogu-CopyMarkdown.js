0c602f9c8ddd4112dc900cf082a57dc7df051323
LYkcul <126674875+LYkcul@users.noreply.github.com>
2023-06-27 19:38:26 +0800

    -version 0.2:添加比赛、训练详情markdown源代码复制

--- Luogu-CopyMarkdown.js
+++ Luogu-CopyMarkdown.js
@@ -1,20 +1,22 @@
 // ==UserScript==
-// @name         主页源代码获取
-// @namespace    https://www.luogu.com.cn/user/486799/
-// @description  获取个人主页源代码
+// @name         Luogu-CopyMarkdown
+// @namespace    https://github.com/Luogu-Extended-Org/Luogu-CopyMarkdown
+// @description  获取洛谷部分页面源代码
 // @author       BlackPanda
 // @license      MIT
 // @version      0.2
-// @match        https://www.luogu.com.cn/user/*
+// @match          https://*.luogu.com.cn/*
+// @match          https://*.luogu.org/*
 // @grant        none
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
+    var url = url = window.location.href;
     async function user_detail(){
         var button = document.createElement('button');
         button.textContent = 'Copy';
-        button.classList.add('red-button', 'button-size');
+        button.classList.add('button', 'button-size');
         button.addEventListener('click', async function() {
             var introduction = _feInstance.currentData.user.introduction;
             await navigator.clipboard.writeText(introduction);
@@ -24,15 +26,63 @@
         var pos = tmp[0];
         pos.appendChild(button);
     };
+    //To do
+    async function blog(){
+        var button = document.createElement('button');
+        button.textContent = 'Copy';
+        button.classList.add('button-blog', 'button-size-blog');
+        button.addEventListener('click', async function() {
+            fetch('/api/blog/detail/' + BlogGlobals.blogID).then(res => res.json()).then(res => navigator.clipboard.writeText(res.data.Content));
+            alert('复制成功');
+        });
+        var pos = document.querySelector('div>h2');
+        pos.insertBefore(button, pos.children[0]);
+    };
+    async function contest_detail(){
+        var button = document.createElement('button');
+        button.textContent = 'Copy';
+        button.classList.add('button', 'button-size');
+        button.addEventListener('click', async function() {
+            var introduction = _feInstance.currentData.contest.description;
+            await navigator.clipboard.writeText(introduction);
+            alert('复制成功');
+        });
+        var tmp = document.getElementsByClassName('card padding-default');
+        var pos = tmp[0];
+        pos.appendChild(button);
+    };
+    async function training_detail(){
+        var button = document.createElement('button');
+        button.textContent = 'Copy';
+        button.classList.add('button', 'button-size');
+        button.addEventListener('click', async function() {
+            var introduction = _feInstance.currentData.training.description;
+            await navigator.clipboard.writeText(introduction);
+            alert('复制成功');
+        });
+        var tmp = document.getElementsByClassName('card padding-default');
+        var pos = tmp[0];
+        pos.appendChild(button);
+    };
     var style = document.createElement('style');
     style.textContent = `
-        .red-button {
+        .button {
             background-color: #6495ed;
             color: #fff;
             border-radius: 6px;
         }
+         .button-blog {
+            background-color: #6495ed;
+            color: #fff;
+            border-radius: 6px;
+            font-size: 15px;
+        }
         .button-size {
-            width: 75px;
+            width: 50px;
+            height: 25px;
+        }
+        .button-size-blog {
+            width: 40px;
             height: 25px;
         }
         * {
@@ -41,6 +91,18 @@
           border: none;
         }
         `;
-    document.head.appendChild(style);
-    window.addEventListener('load', user_detail);
+    if (url.includes('blog')) {
+        //To do
+        //document.head.appendChild(style);
+        //window.addEventListener('load', blog);
+    } else if (url.includes('user')) {
+        document.head.appendChild(style);
+        window.addEventListener('load', user_detail);
+    } else if (url.includes('contest')) {
+        document.head.appendChild(style);
+        window.addEventListener('load', contest_detail);
+    } else if (url.includes('training')) {
+        document.head.appendChild(style);
+        window.addEventListener('load', training_detail);
+    }
 })();
