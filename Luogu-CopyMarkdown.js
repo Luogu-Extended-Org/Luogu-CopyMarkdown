40b81dd0900c65f2152e7e0b6f4f33a1c5f186d9
LYkcul <126674875+LYkcul@users.noreply.github.com>
2023-06-29 16:51:39 +0800

    -version 0.4:button css美化+排除部分页面+bug修复

--- Luogu-CopyMarkdown.js
+++ Luogu-CopyMarkdown.js
@@ -16,15 +16,19 @@
     async function user_detail(){
         var button = document.createElement('button');
         button.textContent = '复制Md';
-        button.classList.add('button', 'button-size');
+        button.style.position = "absolute";
+        button.style.top = "100px";button.style.right = "100px";
+        window.addEventListener('scroll', function() {
+            var scrollY = window.scrollY;
+            button.style.top = (100 + scrollY) + 'px';
+        });
+        button.classList.add('button');
         button.addEventListener('click', async function() {
             var introduction = _feInstance.currentData.user.introduction;
             await navigator.clipboard.writeText(introduction);
             alert('复制成功');
         });
-        var tmp = document.getElementsByClassName('card padding-default');
-        var pos = tmp[0];
-        pos.appendChild(button);
+        document.body.appendChild(button);
     };
     function blog(){
         var button = document.createElement('button');
@@ -35,7 +39,7 @@
             var scrollY = window.scrollY;
             button.style.top = (100 + scrollY) + 'px';
         });
-        button.classList.add('button-blog', 'button-size-blog');
+        button.classList.add('button');
         button.addEventListener('click', async function() {
             fetch('/api/blog/detail/' + BlogGlobals.blogID).then(res => res.json()).then(res => navigator.clipboard.writeText(res.data.Content));
             alert('复制成功');
@@ -45,70 +49,88 @@
     async function contest_detail(){
         var button = document.createElement('button');
         button.textContent = '复制Md';
-        button.classList.add('button', 'button-size');
+        button.style.position = "absolute";
+        button.style.top = "100px";button.style.right = "100px";
+        window.addEventListener('scroll', function() {
+            var scrollY = window.scrollY;
+            button.style.top = (100 + scrollY) + 'px';
+        });
+        button.classList.add('button');
         button.addEventListener('click', async function() {
             var introduction = _feInstance.currentData.contest.description;
             await navigator.clipboard.writeText(introduction);
             alert('复制成功');
         });
-        var tmp = document.getElementsByClassName('card padding-default');
-        var pos = tmp[0];
-        pos.appendChild(button);
+        document.body.appendChild(button);
     };
     async function training_detail(){
         var button = document.createElement('button');
         button.textContent = '复制Md';
-        button.classList.add('button', 'button-size');
+        button.style.position = "absolute";
+        button.style.top = "100px";button.style.right = "100px";
+        window.addEventListener('scroll', function() {
+            var scrollY = window.scrollY;
+            button.style.top = (100 + scrollY) + 'px';
+        });
+        button.classList.add('button');
         button.addEventListener('click', async function() {
             var introduction = _feInstance.currentData.training.description;
             await navigator.clipboard.writeText(introduction);
             alert('复制成功');
         });
-        var tmp = document.getElementsByClassName('card padding-default');
-        var pos = tmp[0];
-        pos.appendChild(button);
+        document.body.appendChild(button);
     };
     var style = document.createElement('style');
     style.textContent = `
         .button {
-            background-color: #6495ed;
-            color: #fff;
-            font-size: 13px;
-        }
-        .button-blog {
-            background-color: #6495ed;
-            color: #fff;
-            font-size: 13px;
-        }
-        .button-blog:hover {
-            background-color: lightgray;
+            outline:none !important;
             cursor: pointer;
+            line-height: 1.25;
+            position: relative;
+            display: block;
+            margin-left: -.0625rem;
+            padding: .5rem .75rem;
+            color: #fff !important;
+            border: .0625rem solid #dee2e6;
+            font-size: 15px;
+            font-weight: unset;
+            display: flex;
+            min-width: 36px;
+            height: 36px;
+            margin: 0 3px;
+            border-radius: 100px!important;
+            align-items: center;
+            justify-content: center;
+            transition:all .3s;
+            background-color: #5e72e4;
         }
-        .button-size {
-            width: 50px;
-            height: 25px;
-        }
-        .button-size-blog {
-            width: 50px;
-            height: 25px;
-        }
-        * {
-          margin: 0;
-          padding: 0;
-          border: none;
+        .button:hover {
+            box-shadow: 0 7px 14px rgba(50,50,93,.1), 0 3px 6px rgba(0,0,0,.08);
+            transform: translateY(-1px);
         }
         `;
     document.head.appendChild(style);
-    if (url.includes('blog')) {
-        blog();
+    if (url.includes('blog') && !url.includes('Admin') && !url.includes('admin')) {
+        var parsedUrl = new URL(url);
+        if (url.includes('org')) {
+            var path = parsedUrl.pathname.split('/');
+            if (path.length >= 2 && path[1] != '') {
+                window.addEventListener('load', blog);
+            }
+        } else {
+            var path = parsedUrl.pathname.split('/');
+            if (path.length >= 4) {
+                window.addEventListener('load', blog);
+            }
+        }
     }
-    if (url.includes('user') && !url.endsWith('#practice') && !url.endsWith('#mine') && !url.endsWith('#problem') && !url.includes('follower') && !url.includes('following') && !url.endsWith('#favorite')) {
+    if (url.includes('user')) {
         window.addEventListener('load', user_detail);
     }
-    if (url.includes('contest') && !url.includes('list') && !url.endsWith('#scoreboard') && !url.includes('edit') && !url.includes('contestId')) {
+    if (url.includes('contest') && !url.includes('list') && !url.includes('edit') && !url.includes('contestId')) {
         window.addEventListener('load', contest_detail);
     }
-    if (url.includes('training') && !url.includes('edit') && !url.endsWith('#problems') && !url.includes('list')) {
+    if (url.includes('training') && !url.includes('edit') && !url.includes('list')) {
         window.addEventListener('load', training_detail);
     }
 })();
